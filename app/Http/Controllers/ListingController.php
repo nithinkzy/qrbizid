faa8f3d3a4349b26687968dbe821be74e5798cb3
Nithin Kumar K.S <nithinkzy@gmail.com>
2023-08-24 20:53:46 -0400

    Initial commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    public function index()
+    {
+        // dd(request()->tag);
+        return view('listings.index', [
+            'listings'  => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
+        ]);
+    }
+
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // Show Create Form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request)
+    {
+        // dd($request->file('logo'));
+
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+        $formFields['user_id'] = auth()->id();
+
+        Listing::create($formFields);
+        return redirect('/')->with('message', 'Listing created successfully !');
+        // dd($request->all());
+    }
+
+    // Show edit form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    // Update listing data
+    public function update(Request $request, Listing $listing)
+    {
+        // dd($request->file('logo'));
+
+        // Make sure logged user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+        return back()->with('message', 'Listing updated successfully !');
+        // dd($request->all());
+    }
+
+    // Delete Listing
+    public function destroy(Listing $listing)
+    {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted succesfully !');
+    }
+
+    // Manage Listing
+    public function manage()
+    {
+        return view('listings.manage', ['listings' => auth()->user()->listings()->get()]);
+    }
+}
