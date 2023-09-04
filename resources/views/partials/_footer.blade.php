57ea873dd5bb7a0e5feeeddfedf0af7083f25943
Nithin Kumar K.S <nithinkzy@gmail.com>
2023-09-04 14:45:38 -0400

    footer full width

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="relative bottom-0 left-0 right-0 bg-white dark:bg-gray-800">
+<footer class="w-full relative bottom-0 left-0 right-0 bg-white dark:bg-gray-800">
     <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
 
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
